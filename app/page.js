2ff5f3304e55d9e4e37f2837dd81c738c79b256d
Anjez Developer <developer@anjez.com>
2025-08-13 16:40:33 +0300

    Add new file

---
+++ app/page.js
@@ -0,0 +1,10 @@
+// C:\Task-Project-V5\Anjez\app\page.js
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold text-blue-600">
+       Anjez مرحباً بك في  
+      </h1>
+    </div>
+  );
+}
